f5f23c36d96483ba4663d3f88abce2c4ad5bdc56
h4lodev <robert.marvel06@gmail.com>
2026-03-16 14:24:11 -0300

    Surface in surface, inception i guess?

--- src/ProjsWidget.tsx
+++ src/ProjsWidget.tsx
@@ -10,7 +10,15 @@ function ProjsWidget(){
                 style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
                 className="fastfetch"
             >
-                <p style={{ color: 'white', fontSize: '2rem' }}>PROJECT CARDS GO HERE WHEN READY</p>
+                <GlassSurface 
+                    width={1000} 
+                    height={380}
+                    borderRadius={50}
+                    style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+                    className="fastfetch"
+                >
+                    <p style={{ color: 'white', fontSize: '2rem' }}>PROJECT CARDS GO HERE WHEN READY</p>
+                </GlassSurface>
             </GlassSurface>
 
         </div>
